cf88b2fdfe73b305c3ece59bc628b0e6fe07aa33
nghia.huynht <nghia.huynht05@gmail.com>
2022-12-08 07:41:43 +0700

    #1 - nghiahuynh create 1st pages css and html

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ import Container from "@mui/material/Container";
 import images from "./images/home-backgroup-card.jpg";
 
 const Item = styled("div")(({ theme }) => ({
-  border: "1px solid",
-  borderColor: theme.palette.mode === "dark" ? "#444d58" : "#ced7e0",
-  padding: theme.spacing(1),
   borderRadius: "4px",
   textAlign: "center",
   height: "100%",
@@ -37,6 +34,7 @@ function App() {
             "justify-content": "center",
             "flex-direction": "column",
             height: "95vh",
+            p: 0,
           }}
         >
           <Grid
@@ -50,20 +48,33 @@ function App() {
               "background-position": "center",
             }}
           >
-            <Item>
-              <div>
-                <span>Name A & Name B</span>
-              </div>
-              <Box
-                sx={{
-                  background: "",
-                  border: "0.1875em solid #0F1C3F",
-                  "border-radius": "50%",
-                  "box-shadow": "0.375em 0.375em 0 0 rgba(15, 28, 63, 0.125)",
-                  height: "5em",
-                  width: "5em",
-                }}
-              ></Box>
+            <Item sx={{ p: 0 }}>
+              <Grid sx={{ p: 0 }}>
+                <Item sx={{ height: "20vh" }}>Charlotte Luna & Madison Paisley</Item>
+                <Item sx={{ height: "35vh" }}>
+                  <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    style={{ minHeight: "35vh" }}
+                  >
+                    <Box
+                      sx={{
+                        background: "",
+                        border: "0.1875em solid #FFFF",
+                        "border-radius": "50%",
+                        "box-shadow":
+                          "0.375em 0.375em 0 0 rgba(15, 28, 63, 0.125)",
+                        height: "15em",
+                        width: "15em",
+                      }}
+                    ></Box>
+                  </Grid>
+                </Item>
+                <Item sx={{ height: "20vh", p: 0 }}></Item>
+              </Grid>
             </Item>
           </Grid>
           <Grid sx={{ "justify-content": "top", height: "10vh", p: 0 }}>
